809a8f2403b53277273daead38d7a32c149056ff
Forwhat4 <15057763977@163.com>
2018-04-26 16:43:26 +0800

    InnerClass

--- src/inner/Outer.java
+++ src/inner/Outer.java
@@ -5,6 +5,10 @@ public class Outer {
     private int a = 1;
     private static int b = 2;
 
+    {
+        System.out.println("outer");
+    }
+
     public void say(){
         System.out.println("hello");
     }
@@ -16,6 +20,11 @@ public class Outer {
     //普通内部类
     public class GeneralInner{
 
+        {
+            System.out.println("GeneralInner");
+        }
+
+
         public void getOM(){
             say();
             speak();
@@ -25,12 +34,24 @@ public class Outer {
     //静态内部类
     public static class StaticInner{
 
+        {
+            System.out.println("StaticInner");
+        }
+
     }
 
     //局部内部类
-    public void method(){
-        class MethodInner{
+    public void method(final int c){
+        final int d = 4;
 
+        class MethodInner{
+            {
+                System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+                System.out.println(d);
+
+            }
         }
     }
 
@@ -38,11 +59,11 @@ public class Outer {
         Outer o = new Outer();
 
         //创建普通内部类对象 Outer.Inner innerObj = outerObj.new Inner() OR Outer.Inner innerObj = new Outer.new Inner()
-        Outer.GeneralInner gi1 = o.new GeneralInner();
-        Outer.GeneralInner gi2 = new Outer().new GeneralInner();
+        //Outer.GeneralInner gi1 = o.new GeneralInner();
+        //Outer.GeneralInner gi2 = new Outer().new GeneralInner();
 
-        gi1.getOM();
+        //gi1.getOM();
 
-
+        Outer.StaticInner si = new StaticInner();
     }
 }
